e45737946b29444829cb77765b8da321ee2c7c02
uvinet <32094657+uvinet@users.noreply.github.com>
2019-07-23 12:12:04 +0300

    Create TThreadSafeQueue.h

---
+++ TThreadSafeQueue.h
@@ -0,0 +1,44 @@
+	template <typename TValue>
+	class TThreadSafeQueue : private std::queue<TValue>
+	{
+	public:
+		/// добавить значение
+		void push(TValue& val) {
+			const CAutoLock locker(&cs_);
+			__super::push(val);
+		}
+		/// извлечь значение
+		bool pop(TValue& val) {
+			const CAutoLock locker(&cs_);
+			if(!__super::empty()) {
+				val = __super::front();
+				__super::pop();
+				return true;
+			}
+			return false;
+		}
+		/// извлечь все значения
+		bool popAll(TThreadSafeQueue& queue) {
+			const CAutoLock locker(&cs_);
+			if(queue.empty() && !__super::empty()) {
+				__super::swap(queue);
+				return true;
+			}
+			return false;
+		}
+		bool front(TValue& val) {
+			const CAutoLock locker(&cs_);
+			if(!__super::empty()) {
+				val = __super::front();
+				return true;
+			}
+			return false;
+		}
+		int size() { 
+			const CAutoLock locker(&cs_);
+			return __super::size();
+		}
+	private:
+		CCriticalSection cs_; 	///< объект синхронизации
+	};
+  
