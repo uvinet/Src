fd03db898f3eb569e5f7e05316eec389ff3c314e
uvinet <32094657+uvinet@users.noreply.github.com>
2019-11-20 15:01:53 +0300

    Emulates 100% CPU utilization
    Uses all supported threads count

---
+++ ResourceEater.cpp
@@ -0,0 +1,40 @@
+// ResourceEater.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <vector>
+#include <conio.h>
+
+static std::atomic<bool> st_bEat = true;
+
+int main()
+{
+	struct EaterPool
+	{
+		EaterPool()
+		{
+			for(unsigned int n=0; n<std::thread::hardware_concurrency(); n++)
+				threads_vector.emplace_back([]{
+					while (st_bEat)
+						;//yum-yum
+				});
+		}
+		~EaterPool()
+		{
+			for (auto& t : threads_vector)
+				t.join();
+		}
+	private:
+		std::vector<std::thread> threads_vector;
+	} ep;
+
+	std::thread([] {
+		printf("Eating...\npress any key to stop");
+		_getch();
+		st_bEat = false;
+	}).join();
+}
